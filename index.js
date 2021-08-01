af8f9f5698bc132530355144053df4a182377e2b
Benis Tambe <will.jenifer@yahoo.com>
2021-07-31 23:39:10 -0400

    commit tree 18293920ec395fb042eb414470e6fd610775baad

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+main.remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = "victory";
+newHeader.innerHTML = "BT is the champion";
+newHeader.className = "victory";
+
+document.body.appendChild(newHeader);
